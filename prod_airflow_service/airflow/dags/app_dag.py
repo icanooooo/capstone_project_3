7bba1ff97a1acb7025bda534b716a567852dc054
Muhammad Ihsan <muhihsan0@outlook.com>
2024-12-12 11:07:20 +0700

    succesfully created tables for library_project

--- prod_airflow_service/airflow/dags/app_dag.py
+++ prod_airflow_service/airflow/dags/app_dag.py
@@ -1,21 +1,11 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
-from helper.postgres_app_helper import quick_command
+from helper.postgres_app_helper import ensure_table
 
-def ensure_table():
-    ensure_table_query = """
-    CREATE TABLE IF NOT EXISTS users_salary (
-        ID VARCHAR(50),
-        NAME VARCHAR(50),
-        AGE INT,
-        JOB VARCHAR(50),
-        INDUSTRY VARCHAR(50),
-        SALARY DOUBLE PRECISION,
-        INPUT_TIME TIMESTAMP    
-    );
-"""
-    quick_command(ensure_table_query, "application_postgres", "5432", "application_db", "library_admin", "letsreadbook")
+def generate_data():
+    ensure_table()
+
 
 with DAG('generate_table_dag',
          start_date=datetime(2024, 12, 11),
@@ -23,4 +13,4 @@ with DAG('generate_table_dag',
          schedule='@hourly',
          catchup=False) as dag:
     
-    create_table = PythonOperator(task_id='create_tables', python_callable=ensure_table)
+    create_table = PythonOperator(task_id='create_tables', python_callable=generate_data)
