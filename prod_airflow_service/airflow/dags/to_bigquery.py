6680a261fca7c4fa9d5fe6127238725afe21429c
Muhammad Ihsan <muhihsan0@outlook.com>
2024-12-16 18:33:14 +0700

    finished creating insert to bq dag, lets test!

--- prod_airflow_service/airflow/dags/to_bigquery.py
+++ prod_airflow_service/airflow/dags/to_bigquery.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from helper.postgres_app_helper import create_connection, print_query
+from helper.bigquery_helper import create_client, load_bigquery
 from airflow.utils.task_group import TaskGroup
 from airflow.operators.python import PythonOperator
 from datetime import datetime
@@ -23,9 +24,21 @@ def ingest_data(source_table, temp_storage):
     temp_file_path = os.path.join(temp_storage, f"{source_table}.csv")
     df.to_csv(temp_file_path, index=False)
 
+def load_data(source_table, temp_storage, project_id, dataset_id, destination):
+    client = create_client()
+    dataframe = pd.read_csv(f"{temp_storage}/{source_table}.csv")
+    table_id = f"{project_id}.{dataset_id}.{destination}"
+
+    load_bigquery(client, dataframe, table_id, "WRITE APPEND", "created_at")
+
+    print(f"loaded {dataframe.shape[0]} row to {destination}")
+
+
 def create_dag():
     config=load_config()
     temp_storage = config["temp_storage"]["location"]
+    project_id = config["bigquery"]["project"]
+    dataset_id = config["bigquery"]["dataset"]
 
     os.makedirs(temp_storage, exist_ok=True)
 
@@ -49,7 +62,19 @@ def create_dag():
                     },
                 )
 
-                ingest_task
+                insert_to_bq=PythonOperator(
+                    task_id=f"loading_{source_table}",
+                    python_callable=load_data,
+                    op_kwargs={
+                        "source_table": source_table,
+                        "temp_storage": temp_storage,
+                        "project_id": project_id,
+                        "dataset_id": dataset_id,
+                        "destination": destination_table
+                    }
+                )
+
+                ingest_task >> insert_to_bq
     
     return dag
 
